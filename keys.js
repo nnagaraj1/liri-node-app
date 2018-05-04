a0b2d7acf0898cf97be60fdbf0d3a806cd49f048
Neeni Nagaraj <neeninagaraj@Neenis-MacBook-Air.local>
2018-05-03 20:31:33 -0400

    updated .gitignore

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
-//console.log('this is loaded');
+console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: "gVTKsvN4piyJb3DxFY2y4rvdT",
-  consumer_secret: "lYQJRV8zJ9RD9cYytmZV8Qc7AOxwzCKlqlzSRPaMlqioAb9yJf",
-  access_token_key: 	"988128827359612928-WUuL190xgexytp6umXFLA7T0kDbmWCL",
-  access_token_secret: "rz4w7RmHvNfbsBGvInx3FuOoHPm68TwFNbAWg4Y4pTiFZ"
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: f4e952a19eae45428b78b4ea76e4b82a,
-  secret: cea2b033af1743fc96c448a67cbd9e8
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
